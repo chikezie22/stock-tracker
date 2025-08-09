3318506b8ded2a7995338853d449d9de736c2f22
Chikezie Simon <simonchikezie44@gmail.com>
2025-08-09 13:59:19 +0100

    env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  finnhubApiKey: 'd2ar5o1r01qgk9ug9ub0d2ar5o1r01qgk9ug9ubg',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  finnhubApiKey: 'd2ar5o1r01qgk9ug9ub0d2ar5o1r01qgk9ug9ubg',
+};
